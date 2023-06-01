f4f90a84e0bdc2909c56929173fd616be550b383
elminm <elmin555@bk.ru>
2023-06-01 17:47:47 +0400

    ..

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+
 mongoose.connect('mongodb+srv://Elminm:jNEw1D2pOtGgGwBg@cluster0.kuxordu.mongodb.net/usersdb');
 
-
-//table - collections
-
-//product adında bir collection design ettim
 let userSchema = new Schema({
     name: String,
     username: String,
     email:String
+})
+let productSchema = new Schema({
+    name: String,
+    unitPrice: Number,
+    unitsInstock: Number,
+    date: { type: Date, default: Date.now}
 })
 
 //user adında bir collection design ettim
@@ -27,10 +30,55 @@ let userSchema = new Schema({
 
 //bu dizaynı mongoya haber verdim.
 // let Product = mongoose.model('Product', productSchema)
+
 let User = mongoose.model('User', userSchema)
+let Products = mongoose.model('Products', productSchema)
 
 
 
+app.get('/products/api', (req, res) => {
+    Products.find()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+app.get('/products/api/:id', (req, res) => {
+    Products.findById(req.params.id)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        res.json(err);
+    })
+})
+app.post('/products/api', (req, res) => {
+
+    let product = new Products({
+        name: req.body.name,
+        unitPrice: req.body.unitPrice,
+        unitsInstock: req.body.unitsInstock
+    })
+    product.save();
+    res.json(product);
+})
+app.put('/products/api/:id', (req, res) => {
+    let id = req.params.id
+    let newProduct = {
+        name:req.body.name,
+        unitPrice:req.body.unitPrice,
+        unitsInstock:req.body.unitsInstock,
+        date: new Date
+    }
+    Products.findByIdAndUpdate(id,newProduct,{new:true}).then(()=>{
+        res.json({msg:'updated'})
+    }).catch(err=>res.json(err))
+})
+
+
+///////////////////////
 app.get('/users/api', (req, res) => {
     User.find()
         .then(data => {
@@ -51,6 +99,7 @@ app.post('/users/api', (req, res) => {
     user.save();
     res.json(user);
 })
+
 app.delete('/users/api/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id)
         .then(data => {
